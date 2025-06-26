ad9de9349fb9d1dfbc57bc7deef7c6a03c77caf1
teplostanski <igor@teplostan.ski>
2025-06-26 21:57:50 +0300

    Migrate from vite-plugin-pretty-module-classnames to vite-plugin-readable-classnames

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
-import PrettyModuleClassnames from 'vite-plugin-pretty-module-classnames'
+import readableClassnames from 'vite-plugin-readable-classnames'
 import sassDts from 'vite-plugin-sass-dts'
 
 // https://vite.dev/config/
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     checker({ typescript: true }),
-    PrettyModuleClassnames(),
+    readableClassnames(),
     sassDts({ enabledMode: ['development', 'production'], esmExport: true }),
   ],
 })
